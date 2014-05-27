f7a8310130ba352f8e68ba5f0d6b179fb63c2c74
Charlie Proctor <charlie@charlieproctor.com>
2014-05-27 14:57:36 -0400

    added easy ruby example

---
+++ lectures/ruby/basic.rb
@@ -0,0 +1,31 @@
+# Some basic ruby examples to show the class:
+
+# Have them start up irb
+
+# Strings:
+""
+" "
+"H"
+"Hi"
+"John"
+"abcd"
+"1234"
+"+++^^^^+++"
+
+# Concatenating Strings:
+"Hi" + " " + "John" ==> "Hi John"
+"YEI" + " " + "Tech" + " " + "Bootcamp" ==> "YEI Tech Bootcamp"
+
+# Numbers:
+5
+24
+24.0
+0.148
+
+# Algebra
+5+5 ==> 10
+6/3 ==> 2
+12 * 4 ==> 48
+
+# Variables
+a = 
